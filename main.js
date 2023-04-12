b23434bbbc0f8ae35510ac076f79ca54a2f4830b
louispaulet <louispaulet13@gmail.com>
2023-04-12 22:49:04 +0200

    add new recipe and create style

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-const recipe = applePieRecipe.recipe;
+const recipes = [
+  { name: "Apple Pie", data: applePieRecipe.recipe },
+  { name: "Chocolate Chip Cookies", data: newRecipe.recipe }
+];
 
 function createIngredientsList(ingredients) {
   const list = document.createElement("ul");
+  list.classList.add("list-unstyled");
   ingredients.forEach(ingredient => {
     const listItem = document.createElement("li");
     listItem.innerText = `${ingredient.quantity} ${ingredient.unit} ${ingredient.name}`;
@@ -15,34 +19,74 @@ function createIngredientsList(ingredients) {
 
 function createInstructionsList(instructions) {
   const list = document.createElement("ol");
+  list.classList.add("list-group", "list-group-numbered");
   instructions.forEach(instruction => {
     const listItem = document.createElement("li");
+    listItem.classList.add("list-group-item");
     listItem.innerText = instruction;
     list.appendChild(listItem);
   });
   return list;
 }
 
+function displayRecipeDropdown() {
+  const container = document.getElementById("recipe-dropdown");
+  const select = document.createElement("select");
+  select.classList.add("form-select");
+
+  recipes.forEach((recipe, index) => {
+    const option = document.createElement("option");
+    option.value = index;
+    option.innerText = recipe.name;
+    select.appendChild(option);
+  });
+
+  select.addEventListener("change", (e) => {
+    const recipeIndex = e.target.value;
+    displayRecipe(recipes[recipeIndex].data);
+  });
+
+  container.appendChild(select);
+}
+
+function clearRecipeContainer() {
+  const container = document.getElementById("recipe-container");
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
 function displayRecipe(recipe) {
+  clearRecipeContainer();
+  
   const container = document.getElementById("recipe-container");
 
   const title = document.createElement("h1");
+  title.classList.add("display-4", "mb-4");
   title.innerText = recipe.title;
   container.appendChild(title);
 
+  const detailsRow = document.createElement("div");
+  detailsRow.classList.add("row", "mb-4");
+  container.appendChild(detailsRow);
+
   const yieldInfo = document.createElement("p");
+  yieldInfo.classList.add("col-sm", "text-center");
   yieldInfo.innerText = `Yield: ${recipe.yield.quantity} ${recipe.yield.unit}`;
-  container.appendChild(yieldInfo);
+  detailsRow.appendChild(yieldInfo);
 
   const prepTime = document.createElement("p");
+  prepTime.classList.add("col-sm", "text-center");
   prepTime.innerText = `Prep Time: ${recipe.prep_time.quantity} ${recipe.prep_time.unit}`;
-  container.appendChild(prepTime);
+  detailsRow.appendChild(prepTime);
 
   const cookTime = document.createElement("p");
+  cookTime.classList.add("col-sm", "text-center");
   cookTime.innerText = `Cook Time: ${recipe.cook_time.quantity} ${recipe.cook_time.unit}`;
-  container.appendChild(cookTime);
+  detailsRow.appendChild(cookTime);
 
   const ingredientsHeader = document.createElement("h2");
+  ingredientsHeader.classList.add("mb-3");
   ingredientsHeader.innerText = "Ingredients";
   container.appendChild(ingredientsHeader);
 
@@ -50,6 +94,7 @@ function displayRecipe(recipe) {
   container.appendChild(ingredientsList);
 
   const instructionsHeader = document.createElement("h2");
+  instructionsHeader.classList.add("mb-3");
   instructionsHeader.innerText = "Instructions";
   container.appendChild(instructionsHeader);
 
@@ -57,4 +102,5 @@ function displayRecipe(recipe) {
   container.appendChild(instructionsList);
 }
 
-displayRecipe(recipe);
+displayRecipeDropdown();
+displayRecipe(recipes[0].data);

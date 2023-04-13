d60b67766ffa54a7095cabcbb49cc0cf1769fc67
louispaulet <louispaulet13@gmail.com>
2023-04-13 02:03:03 +0200

    refactor recipe list

--- recipe.js
+++ recipe.js
@@ -82,6 +82,165 @@ const applePieRecipe = {
   }
 };
 
+const newRecipe = {
+  "recipe": {
+    "title": "Chocolate Chip Cookies",
+    "yield": {
+      "quantity": 24,
+      "unit": "cookies"
+    },
+    "prep_time": {
+      "quantity": 15,
+      "unit": "minutes"
+    },
+    "cook_time": {
+      "quantity": 10,
+      "unit": "minutes"
+    },
+    "ingredients": [
+      {
+        "name": "all-purpose flour",
+        "quantity": 2.25,
+        "unit": "cups"
+      },
+      {
+        "name": "baking soda",
+        "quantity": 0.5,
+        "unit": "teaspoon"
+      },
+      {
+        "name": "unsalted butter",
+        "quantity": 1,
+        "unit": "cup",
+        "preparation": "softened"
+      },
+      {
+        "name": "granulated sugar",
+        "quantity": 0.75,
+        "unit": "cup"
+      },
+      {
+        "name": "brown sugar",
+        "quantity": 0.75,
+        "unit": "cup"
+      },
+      {
+        "name": "vanilla extract",
+        "quantity": 1,
+        "unit": "teaspoon"
+      },
+      {
+        "name": "large eggs",
+        "quantity": 2
+      },
+      {
+        "name": "semi-sweet chocolate chips",
+        "quantity": 2,
+        "unit": "cups"
+      }
+    ],
+    "instructions": [
+      "Preheat oven to 375°F (190°C).",
+      "In a small bowl, whisk together the flour and baking soda; set aside.",
+      "In a large bowl, beat the butter, granulated sugar, brown sugar, and vanilla extract with an electric mixer until creamy.",
+      "Add the eggs, one at a time, beating well after each addition.",
+      "Gradually beat in the flour mixture.",
+      "Stir in the chocolate chips.",
+      "Drop dough by rounded tablespoonfuls onto ungreased cookie sheets.",
+      "Bake for 9 to 11 minutes, or until golden brown.",
+      "Cool on wire racks."
+    ]
+  }
+};
+
+const europeanApplePieRecipe = {
+  "recipe": {
+    "title": "European Apple Pie",
+    "yield": {
+      "quantity": 8,
+      "unit": "servings"
+    },
+    "prep_time": {
+      "quantity": 30,
+      "unit": "minutes"
+    },
+    "cook_time": {
+      "quantity": 40,
+      "unit": "minutes"
+    },
+    "ingredients": [
+      {
+        "name": "all-purpose flour",
+        "quantity": 300,
+        "unit": "g"
+      },
+      {
+        "name": "granulated sugar",
+        "quantity": 150,
+        "unit": "g"
+      },
+      {
+        "name": "unsalted butter",
+        "quantity": 200,
+        "unit": "g",
+        "preparation": "chilled, cut into small pieces"
+      },
+      {
+        "name": "cold water",
+        "quantity": 60,
+        "unit": "ml"
+      },
+      {
+        "name": "apples",
+        "quantity": 1000,
+        "unit": "g",
+        "preparation": "peeled, cored, and sliced"
+      },
+      {
+        "name": "lemon juice",
+        "quantity": 15,
+        "unit": "ml"
+      },
+      {
+        "name": "brown sugar",
+        "quantity": 100,
+        "unit": "g"
+      },
+      {
+        "name": "cornstarch",
+        "quantity": 15,
+        "unit": "g"
+      },
+      {
+        "name": "ground cinnamon",
+        "quantity": 2,
+        "unit": "g"
+      },
+      {
+        "name": "vanilla extract",
+        "quantity": 5,
+        "unit": "ml"
+      },
+      {
+        "name": "salt",
+        "quantity": 1,
+        "unit": "g"
+      }
+    ],
+    "instructions": [
+      "In a large bowl, combine the flour, 100g of granulated sugar, and salt. Add the chilled butter and mix until the mixture resembles coarse crumbs. Gradually add cold water and mix until the dough comes together. Divide the dough in half, wrap each half in plastic wrap, and refrigerate for at least 1 hour.",
+      "Preheat the oven to 200°C (390°F).",
+      "In a large bowl, combine the apples, lemon juice, brown sugar, cornstarch, cinnamon, vanilla extract, and a pinch of salt. Mix well and set aside.",
+      "On a lightly floured surface, roll out one half of the dough to a 30 cm (12-inch) circle. Transfer the dough to a 24 cm (9.5-inch) pie dish. Trim the excess dough, leaving a 2 cm (1-inch) overhang.",
+      "Fill the pie crust with the apple mixture.",
+      "Roll out the second half of the dough to a 30 cm (12-inch) circle. Place it over the apple filling. Trim the excess dough, leaving a 2 cm (1-inch) overhang. Fold the top crust under the bottom crust and pinch the edges to seal. Cut a few slits in the top crust to allow steam to escape.",
+      "Sprinkle the remaining 50g of granulated sugar over the top crust.",
+      "Bake the pie in the preheated oven for 15 minutes. Then, reduce the temperature to 180°C (350°F) and continue baking for 25-30 minutes, or until the crust is golden brown and the filling is bubbly. If the edges of the crust start to brown too quickly, cover them with aluminum foil.",
+      "Remove the pie from the oven and let it cool on a wire rack for at least 1 hour before serving. This will allow the filling to set and make it easier to slice.",
+      "Serve the European Apple Pie warm or at room temperature with a scoop of vanilla ice cream or a dollop of whipped cream, if desired."
+    ]}
+};
+
 
 const truffledBresseChickenRecipe = {
 "recipe": {
